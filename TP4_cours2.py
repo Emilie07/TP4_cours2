29b27a621ec959db049c7983ca628d124cb1a9cf
Emilie07 <manceramontanoe@educ.college-montreal.qc.ca>
2023-03-21 10:36:50 -0400

    cours 2

---
+++ TP4_cours2.py
@@ -0,0 +1,61 @@
+#Emilie Mancera
+
+import arcade
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+COLORS = [arcade.color.AFRICAN_VIOLET, arcade.color.AMARANTH_PINK, arcade.color.ARYLIDE_YELLOW, arcade.color.BLEU_DE_FRANCE]
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
+        super().__init__(width, height)
+        self.liste.balles = []
+        self.liste.rectangles = []
+        def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                self.liste.balles.append(balle())
+                pass
+            if button == arcade.MOUSE_BUTTON_RIGHT:
+                self.liste.rectangle.append(rectangle())
+                pass
+
+def balle(self):
+    for _ in range (20):
+
+        def setup(self):
+            pass
+
+        def on_update(self, delta_time: float):
+            if balle_x < rayon.cercles:
+                balle_x *= -1
+            if balle_x > SCREEN_WIDTH - rayon_balle:
+                balle_x *= -1
+            if balle_y < rayon.cercles:
+                balle_y *= -1
+            if balle_y > SCREEN_HEIGHT - rayon_balle:
+                balle_y *= -1
+
+        def on_draw(self):
+            arcade.start_render()
+            arcade.draw_circle_filled(10,10,20, (255, 54, 34))
+
+def rectangle(self):
+    def setup(self):
+        pass
+
+    #def on_update(self delta_time: float):
+        #pass
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_rectangle_filled(200, SCREEN_HEIGHT / 2, 30, 60,
+                                     arcade.csscolor.pass)
+
+def main():
+    my_game = MyGame()
+    my_game.setup()
+
+    arcade.run()
